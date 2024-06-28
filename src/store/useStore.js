41de414176b7e1ba85c29257f04ad240ee87103d
Marcos Daniel Mercado <marcosmerk156@gmail.com>
2024-06-28 15:12:39 -0300

    feat: Update useStore to store user information
    This commit updates the `useStore` hook to store the user information in the state. It adds the `user` property to the state object and initializes it with the value retrieved from `localStorage`. It also adds the `setUser` function to update the user information and the `clearAuth` function to clear both the authentication token and the user information from the state. Additionally, it adds the `users` array to the state and the `setUsers` function to update it.

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -1,11 +1,19 @@
 import { create } from "zustand";
 
 const useStore = create((set) => ({
-  authToken: null,
-  setAuthToken: (token) => set({ authToken: token }),
-  clearAuthToken: () => set({ authToken: null }),
+  authToken: localStorage.getItem("authToken") || null,
+  user: JSON.parse(localStorage.getItem("user")) || null,
 
+  setAuthToken: (token) => set({ authToken: token }),
+  setUser: (user) => set({ user }),
+
+  clearAuth: () => set({ authToken: null, user: null }),
   posts: [],
+  users: [],
+  setPosts: (posts) => {
+    set({ posts });
+  },
+  setUsers: (users) => set({ users }),
   addPost: (post) => set((state) => ({ posts: [...state.posts, post] })),
   getPostById: (id) => (state) => state.posts.find((post) => post.id === id),
 }));
